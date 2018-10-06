f93f2c85004f6c7d2603092918b7eb5a5850a38c
Rachid <rachid845@yahoo.it>
2018-10-06 20:35:47 +0200

    solved Recursion: Davis Staircase: https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking

---
+++ delete/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 01.6
+	fmt.Printf("hello \"Rachid\" \\world!\n %v \n", x)
+
+	var rac int
+	fmt.Println(rac)
+	rac = 20
+	fmt.Println(rac)
+	raci := nil
+	fmt.Println(rac)
+}
