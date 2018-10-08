621cc38fcd26cb4a83006c9303139f899104b166
Rachid <rachid845@yahoo.it>
2018-10-08 18:50:39 +0200

    Resolved Max Array Sum: https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming

--- delete/main.go
+++ delete/main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	x := 01.6
-	fmt.Printf("hello \"Rachid\" \\world!\n %v \n", x)
-
-	var rac int
-	fmt.Println(rac)
-	rac = 20
-	fmt.Println(rac)
-	raci := nil
-	fmt.Println(rac)
+	var s []int
+	//s := []int{}
+	fmt.Printf("%v \n %v", s, s)
+	if s == nil {
+		fmt.Println("is nil")
+	}
+	filter := [10]float32{-1, 4: -0.1, -0.1, 9: -1}
+	fmt.Println(filter)
 }
