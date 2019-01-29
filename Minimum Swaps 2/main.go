a1a7c4b3516995e175d7cdd5c49bc93c25f73068
rachid845@yahoo.it <rachid845@yahoo.it>
2019-01-29 20:55:02 +0100

    resolved exercises Minimum Swaps 2 : https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays

---
+++ Minimum Swaps 2/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the minimumSwaps function below.
+func minimumSwaps(arr []int32) int32 {
+
+	sizeA := int32(len(arr))
+	var tot int32
+	//fmt.Println(arr)
+	for i := int32(0); i < sizeA; i++ {
+		tmp := arr[i]
+		if tmp != i+1 {
+			arr[i], arr[tmp-1] = arr[tmp-1], arr[i]
+			i--
+			tot++
+
+		}
+	}
+
+	//fmt.Println(arr, tot)
+	return tot
+
+}
+
+func main() {
+
+	f, err := os.Open("input.txt")
+	checkError(err)
+	defer f.Close()
+	reader := bufio.NewReaderSize(f, 1024*1024)
+
+	stdout, err := os.Create("OUTPUT_PATH")
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	res := minimumSwaps(arr)
+
+	fmt.Fprintf(writer, "%d\n", res)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
